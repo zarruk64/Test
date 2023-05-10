56323b940df80307a352b04076f29961d0cc2490
zarruk64 <zarruk64@gmail.com>
2023-05-09 22:36:38 -0500

    Progreso 05/09/2023-22:36

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import screens
 
 running = True
 playing = False
+dead = False
+mapa = 1
+maxWaves = {"1": 5, "2": 5, "3": 10, "4": 10, "5": 15, "6": 15, "7": 20, "8": 20}
+wave = 1
+enemies = 5
+gender = ""
+character = ""
+numChar = 1
 num = 1
 time_1 = 0
 displayScreen = "main"
@@ -51,11 +59,46 @@ while running:
                     else:
                         btn = 2
                 elif event.key == pygame.K_RETURN:
-                    if btn == 3:
+                    if btn == 1:
+                        gender = "Woman"
+                        displayScreen = "character"
+                    elif btn == 2:
+                        gender = "Man"
+                        displayScreen = "character"
+                    else:
                         displayScreen = "main"
                         btn = 1
+            elif displayScreen == "character":
+                if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                    if num > 1:
+                        num -= 1
                     else:
-                        displayScreen = "dead"
+                        num = 3
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                    if num < 3:
+                        num += 1
+                    else:
+                        num = 1
+                elif event.key == pygame.K_RETURN:
+                    if btn == 1 or btn == 2 or btn == 3:
+                        character = gender + "/" + gender + "_" + str(num)
+                        displayScreen = "game"
+                    else:
+                        displayScreen = "gender"
+                        btn = 1
+            elif displayScreen == "game":
+                if event.key == pygame.K_q:
+                    if wave >= maxWaves[str(mapa)]:
+                        if mapa >= 8:
+                            mapa = 1
+                        else:
+                            mapa += 1
+                        wave = 1
+                    else:
+                        wave += 1
+                elif event.key == pygame.K_e:
+                    dead = True
+                    displayScreen = "dead"
     if not playing:
         if time_1 == 0:
             time_1 = time.time()
@@ -71,6 +114,10 @@ while running:
             screens.mainScreen(screen, font, backgrounds, num, btn)
         elif displayScreen == "gender":
             screens.genderChooseScreen(screen, font, btn)
+        elif displayScreen == "character":
+            screens.characterChooseScreen(screen, font, gender)
+        elif displayScreen == "game":
+            screens.gameScreen(screen, font, backgrounds, mapa, wave, enemies, character)
         elif displayScreen == "dead":
-            screens.deadScreen(screen, font)
+            screens.deadScreen(screen, font, character)
     pygame.display.update()
--- screens.py
+++ screens.py
@@ -21,7 +21,7 @@ def mainScreen(screen, font, backgrounds, num, btn):
 def genderChooseScreen(screen, font, btn):
     screen.blit(pygame.transform.scale(pygame.image.load("data/Nature/nature_6/2.png"), (1400, 750)), (0, 0))
     screen.blit(pygame.transform.scale(pygame.image.load("data/Nature/nature_7/2.png"), (1400, 750)), (0, 0))
-    screen.blit(pygame.image.load("data/Players/Man/Warrior_2/Idle.png"), (825, 275), (96, 96, 240, 240))
+    screen.blit(pygame.image.load("data/Players/Man/Man_2/Idle.png"), (825, 275), (96, 96, 240, 240))
     screen.blit(pygame.transform.scale(pygame.image.load("data/Other/Buttons/button.png"), btnSize), (400, 500))
     screen.blit(pygame.transform.scale(pygame.image.load("data/Other/Buttons/button.png"), btnSize), (800, 500))
     screen.blit(pygame.transform.scale(pygame.image.load("data/Other/Buttons/button.png"), miniBtnSize), (1200, 675))
@@ -31,11 +31,22 @@ def genderChooseScreen(screen, font, btn):
         screen.blit(pygame.transform.scale(pygame.image.load("data/Other/Buttons/Highlight.png"), hlSize), (795, 495))
     elif btn == 3:
         screen.blit(pygame.transform.scale(pygame.image.load("data/Other/Buttons/Highlight.png"), miniHlSize), (1198, 673))
+    screen.blit(pygame.transform.scale(font.render('Choose The Gender\nOf Your Character', True, txtColor), (600, 100)), (400, 50))
     screen.blit(font.render('Male', True, txtColor), (850, 525))
     screen.blit(font.render('Female', True, txtColor), (429, 525))
     screen.blit(pygame.transform.scale(font.render('Back', True, txtColor), (80, 46)), (1210, 677))
+
+def characterChooseScreen(screen, font, gender):
+    screen.blit(pygame.transform.scale(pygame.image.load("data/Nature/nature_6/2.png"), (1400, 750)), (0, 0))
+    screen.blit(pygame.transform.scale(pygame.image.load("data/Nature/nature_7/2.png"), (1400, 750)), (0, 0))
     
-def deadScreen(screen, font):
+def gameScreen(screen, font, backgrounds, mapa, wave, enemies, gender):
+    screen.blit(pygame.transform.scale(backgrounds["nature_" + str(mapa)], (1400, 750)), (0, 0))
+    screen.blit(font.render('Wave: ' + str(wave), True, txtColor), (1200, 25))
+    if enemies == 0:
+        wave += 1
+    
+def deadScreen(screen, font, charcter):
     screen.fill((64, 64, 64))
-    #Dead picture of character
+    screen.blit(pygame.transform.scale(pygame.image.load("data/Players/" + charcter + "/Dead.png"), (240, 1200)), (100, 200))
     screen.blit(pygame.transform.scale(font.render('You Are Dead', True, txtColor, (255, 255, 255, 255)), (600, 100)), (400, 425))
